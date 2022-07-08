f707c183242ea4c92e0b6e57a05ab9468b89ab02
joyeajin <dpwls31200@gmail.com>
2022-07-08 10:20:34 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Nav from "./components/Nav";
+import ProductList from "./components/ProductList";
+import CartList from "./components/CartList";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<ProductList />} />
+          <Route path="/cart" element={<CartList />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import "./Nav.css";
+
+const Nav = () => {
+    const cart = useSelector(state=>state.cart)
+    return (
+        <nav className="nav">
+            <span className="nav-title"><Link to='/'>SHOP</Link></span>
+            <span>
+                <Link to='/cart'>
+                    <i className="fas fa-shopping-cart">
+                        <div className="cart-amount">{cart.length}</div>
+                    </i>
+                </Link>
+            </span>
+        </nav>
+    );
+};
+
+export default Nav;
